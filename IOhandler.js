d622ac02942173b6f0e6f57d60938d0897157051
SEJIN OH <SJLindsay@SJs-Macbook-Air.local>
2021-11-17 21:39:10 -0800

    really complete

--- IOhandler.js
+++ IOhandler.js
@@ -2,88 +2,104 @@
  * Project: COMP1320 Milestone 1
  * File Name: IOhandler.js
  * Description: Collection of functions for files input/output related operations
- * 
- * Created Date: 
- * Author: 
- * 
+ *
+ * Created Date: Nov 4 2021
+ * Author: Sejin Oh
+ *
  */
 
-const unzipper = require('unzipper'),
-  fs = require("fs"),
-  PNG = require('pngjs').PNG,
-  path = require('path');
+const { resolve } = require("path");
+const unzipper = require("unzipper"),
+fsc = require("fs"),
+fs = require("fs").promises,
+PNG = require("pngjs").PNG,
+path = require("path");
 
+function checkExists(grayscaled) {
+  return new Promise((resolve, reject) => {
+    fs.access(grayscaled)
+    .then(() => resolve())
+    .catch(() => {
+      fs.mkdir(grayscaled) 
+        .then(() => resolve())
+        .catch((err) => reject(err))
+    }
+  )}
+)};
 
 /**
- * Description: decompress file from given pathIn, write to given pathOut 
- *  
- * @param {string} pathIn 
- * @param {string} pathOut 
+ * Description: decompress file from given pathIn, write to given pathOut
+ *
+ * @param {string} pathIn
+ * @param {string} pathOut
  * @return {promise}
  */
 const unzip = (pathIn, pathOut) => {
-  fs.createReadStream('myfile.zip')
-    .pipe(unzipper.Extract({ path: 'unzipped' }));
-  console.log("Extraction operation complete");  
+  return fsc
+    .createReadStream(pathIn)
+    .pipe(unzipper.Extract({ path: pathOut }))
+    .promise();
 };
 
 /**
- * Description: read all the png files from given directory and return Promise containing array of each png file path 
- * 
- * @param {string} path 
+ * Description: read all the png files from given directory and return Promise containing array of each png file path
+ *
+ * @param {string} path
  * @return {promise}
  */
-const readDir = dir => {
- return new Promise(function(resolve, reject) {
-  fs.readdir(dir, function(err, files) {
- if (err)
-    reject(err);
- else
-  files.shift();
-    resolve(files);
-    });
+// if you see "this" then don't use arrow function
+const readDir = (dir) => {
+  return new Promise((resolve, reject) => { 
+    fs.readdir(dir)
+      .then((files) => {
+        const pngArr = [];
+        for (const png of files) {
+          if (path.extname(png) === (".png")) {
+            pngArr.push(png);
+          }
+        }
+        resolve(pngArr);
+      })
+      .catch((err) => reject(err));
   });
- };
-// readDir('unzipped').then(files => console.log(files));
+};
 
 /**
- * Description: Read in png file by given pathIn, 
+ * Description: Read in png file by given pathIn,
  * convert to grayscale and write to given pathOut
- * 
- * @param {string} filePath 
- * @param {string} pathProcessed 
+ *
+ * @param {string} filePath
+ * @param {string} pathProcessed
  * @return {promise}
  */
-const grayScale = (pathIn, pathOut) => {
-  fs.createReadStream(pathIn)
-  .pipe(
-    new PNG({
-      filterType: 4,
-    })
-  )
-  .on("parsed", function () {
-    for (var y = 0; y < this.height; y++) {
-      for (var x = 0; x < this.width; x++) {
-        var idx = (this.width * y + x) << 2;
- 
-        // invert color
-        this.data[idx] = 255 - this.data[idx];
-        this.data[idx + 1] = 255 - this.data[idx + 1];
-        this.data[idx + 2] = 255 - this.data[idx + 2];
- 
-        // and reduce opacity
-        this.data[idx + 3] = this.data[idx + 3] >> 1;
-      }
-    }
- 
-    this.pack().pipe(fs.createWriteStream(pathOut));
-  });
-};
 
-grayScale("./unzipped/in.png", "sejin.png")
+
+const grayScale = (pathIn, pathOut) => {
+  fsc
+    .createReadStream(pathIn) // in.png
+    .pipe(new PNG())
+    .on("parsed", function () {
+      for (var y = 0; y < this.height; y++) {
+        for (var x = 0; x < this.width; x++) {
+          var idx = (this.width * y + x) << 2;
+          let gray = (this.data[idx] + this.data[idx + 1] + this.data[idx + 2]) / 3;
+          
+          this.data[idx] = this.data[idx + 1] = this.data[idx + 2] = gray
+        
+
+        }
+      }
+
+      this.pack().pipe(fsc.createWriteStream(pathOut));
+    })
+    .on("error", (_, err) => {
+      console.log(err);
+    });
+};
 
 module.exports = {
   unzip,
   readDir,
-  grayScale
+  grayScale,
+  checkExists
 };
--- main.js
+++ main.js
@@ -1,16 +1,26 @@
 /*
  * Project: COMP1320 Milestone 1
  * File Name: main.js
- * Description: 
- * 
+ * Description:
+ *
  * Created Date: November 4 2021
  * Author: Sejin Oh
- * 
+ *
  */
 
 const IOhandler = require("./IOhandler"),
+  fs = require("fs").promises,
   zipFilePath = `${__dirname}/myfile.zip`,
   pathUnzipped = `${__dirname}/unzipped`,
   pathProcessed = `${__dirname}/grayscaled`;
 
+
+IOhandler.checkExists(pathProcessed) 
+.then(() => IOhandler.unzip(zipFilePath, pathUnzipped))
+.then(() => IOhandler.readDir(pathUnzipped))
+.then((pngArr) => {
+  for (const pic of pngArr)
+  IOhandler.grayScale(`${pathUnzipped}/${pic}`, `${pathProcessed}/NEW${pic}`)
+})
+.catch((err) => console.log(err));
 
